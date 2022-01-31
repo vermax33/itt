23a94609efc127a1a9c43ba0b36e45bd53d70cb3
vermax33 <98722757+vermax33@users.noreply.github.com>
2022-01-31 13:04:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,13 +10,17 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-
+    
+async def on_command_error(ctx2, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error2_msg)
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
     
-async def aaa(ctx):
+async def aaa(ctx2):
     await ctx.send('https://livedoor.blogimg.jp/nim_2525/imgs/e/d/ed590862.jpg')
     
     
